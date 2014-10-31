012e2bbe8764541ef997f9e12b91644b09dafc60
Karg Weng <kargweng@gmail.com>
2014-11-01 01:53:39 +0800

    Added confirmable.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,9 +15,13 @@
 #  last_sign_in_ip        :inet
 #  created_at             :datetime
 #  updated_at             :datetime
+#  confirmation_token     :string(255)
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
 #
 # Indexes
 #
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
